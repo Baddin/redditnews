f8063ee1aaef0240b83154e9ba2bf6d38981e59c
Baddin <baddin2016@gmail.com>
2018-02-08 00:43:15 -0800

    Changed the result to be readable

--- redditnews.go
+++ redditnews.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
+
+type Item struct {
+	Author string `json:"author"`
+	Score  int    `json:"score"`
+	URL    string `json:"url"`
+	Title  string `json:"title"`
+}
+
+type response struct {
+	Data1 struct {
+		Children []struct {
+			Data2 Item `json:"data"`
+		} `json:"children"`
+	} `json:"data"`
+}
 
 func main() {
 	resp, err := http.Get("https://www.reddit.com/r/golang.json")
@@ -19,8 +34,15 @@ func main() {
 		log.Fatal(resp.Status)
 	}
 
-	_, err = io.Copy(os.Stdout, resp.Body)
-	if err != nil {
-		log.Fatal(err)
+	r := new(response)
+	err = json.NewDecoder(resp.Body).Decode(r)
+
+	for _, child := range r.Data1.Children {
+		fmt.Println(child.Data2)
+		fmt.Println(child.Data2.Author)
+		fmt.Println(child.Data2.Score)
+		fmt.Println(child.Data2.URL)
+		fmt.Println(child.Data2.Title)
 	}
+
 }
